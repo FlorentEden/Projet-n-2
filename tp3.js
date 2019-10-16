2d434159f884904f3bc5b4ab98df27ec6c2816c3
FlorentEden <fmartinez@edenschool.fr>
2019-10-16 16:24:30 +0200

    Site

---
+++ tp3.js
@@ -0,0 +1,9 @@
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.filtre');
+const hero = document.querySelector('.background-color');
+const hero = document.querySelector('.background-color');
+const hero = document.querySelector('.background-color');
+
+const tl = new Timelinemax();
+
+tl.fromTo(hero,1, {height: "0%"}, {height: '80%'})
